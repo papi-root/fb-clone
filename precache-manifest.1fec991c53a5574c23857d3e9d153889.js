2146789450b6e67c6a12d291726e24e53bffed93
papi-root <papemorlo@hotmail.fr>
2020-10-16 18:02:13 +0000

    Updates

---
+++ precache-manifest.1fec991c53a5574c23857d3e9d153889.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "120dfbf851f252cec5f066f020022e8a",
+    "url": "/fb-clone/index.html"
+  },
+  {
+    "revision": "249320ed6c754cac7464",
+    "url": "/fb-clone/static/css/main.ef0513d7.chunk.css"
+  },
+  {
+    "revision": "237238c79acf67bcaab8",
+    "url": "/fb-clone/static/js/2.dd94072b.chunk.js"
+  },
+  {
+    "revision": "b60ce777028b77bebc6ee788d6b419c9",
+    "url": "/fb-clone/static/js/2.dd94072b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "249320ed6c754cac7464",
+    "url": "/fb-clone/static/js/main.6dd56771.chunk.js"
+  },
+  {
+    "revision": "eb7d50e9ce207f1d2fa2",
+    "url": "/fb-clone/static/js/runtime-main.27777e45.js"
+  },
+  {
+    "revision": "c3b68e5dcb12bcec47f16745d4cf47e4",
+    "url": "/fb-clone/static/media/bg1.c3b68e5d.jpeg"
+  },
+  {
+    "revision": "005cde6952d0d5e56ffe8c8f5b80b57a",
+    "url": "/fb-clone/static/media/bg2.005cde69.jpeg"
+  },
+  {
+    "revision": "9420edd7e0c8d9f7f57c2c147fdef0ee",
+    "url": "/fb-clone/static/media/bg3.9420edd7.jpeg"
+  },
+  {
+    "revision": "a90268f06cd64f3e61478a58606dab30",
+    "url": "/fb-clone/static/media/fb.a90268f0.jpg"
+  },
+  {
+    "revision": "9bcb94c21df4dbd89eacdc6d677593ed",
+    "url": "/fb-clone/static/media/me.9bcb94c2.jpg"
+  },
+  {
+    "revision": "c1f73371971accedbe2ff6ffdf37b20f",
+    "url": "/fb-clone/static/media/me2.c1f73371.jpg"
+  },
+  {
+    "revision": "19f02afebb8fc8f63cd1b6658857936b",
+    "url": "/fb-clone/static/media/me3.19f02afe.jpg"
+  }
+]);
